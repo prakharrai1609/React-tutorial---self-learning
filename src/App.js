6b7c117f563c629846eb0872bd9c37d8cf73b655
Prakhar <rai.prakhar1609@gmail.com>
2022-12-24 14:36:11 +0530

    learn jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import { useState } from "react";
+import React, { useState } from "react";
+
+
+function greet() {
+  let first_name = prompt("Enter your first name")
+  let last_name = prompt("Enter your last name")
+
+  return `Hey ${first_name} ${last_name}`
+}
 
 function App() {
-  console.log(2)
-  const [count, set] = useState(0)
+
+  // Problem with this : when prompt is called, the page gets rerendered , hence creates an infinite loop.
+
+  console.log(1)
+  // greeting = greet()
+
+
+  const element1 = {
+    type: 'h1',
+    props: {
+      // className: 'greeting',
+      children: 'Hello, world!'
+    }
+  };
+
+  const element2 = React.createElement(
+    'h1',
+    {className: 'greeting'},
+    'Hello, world!'
+  );
+
   return (
     <>
-      <h1>Your new react app...</h1>
-      <button type="submit" onClick={() => {set(count + 1)}}>Clicked {count} times</button>
+      <h1>Test env</h1>
+      {/* <section>{greet()}</section> */}
+
+      {/* {element1} */}
+      {/* ^ causes error */}
+
+      {element2}
     </>
   );
 }
