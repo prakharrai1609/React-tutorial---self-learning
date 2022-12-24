e4b0dfb441a7680588d03c58de0743e48c81b58f
Prakhar <rai.prakhar1609@gmail.com>
2022-12-24 14:00:15 +0530

    usestate hook basics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 
 function App() {
+  console.log(2)
+  const [count, set] = useState(0)
   return (
     <>
       <h1>Your new react app...</h1>
+      <button type="submit" onClick={() => {set(count + 1)}}>Clicked {count} times</button>
     </>
   );
 }
