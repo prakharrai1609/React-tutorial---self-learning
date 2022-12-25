e543e5fb57ab5214a3f23c2326a171eca516738b
Prakhar <rai.prakhar1609@gmail.com>
2022-12-25 13:33:07 +0530

    render in react

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
-import React, { useState } from "react";
-
-
-function greet() {
-  let first_name = prompt("Enter your first name")
-  let last_name = prompt("Enter your last name")
-
-  return `Hey ${first_name} ${last_name}`
-}
+import React, { useEffect, useState } from "react";
 
 function App() {
 
-  // Problem with this : when prompt is called, the page gets rerendered , hence creates an infinite loop.
+  const [time, interval] = useState(new Date().toLocaleTimeString())
 
-  console.log(1)
-  // greeting = greet()
-
-
-  const element1 = {
-    type: 'h1',
-    props: {
-      // className: 'greeting',
-      children: 'Hello, world!'
-    }
-  };
-
-  const element2 = React.createElement(
-    'h1',
-    {className: 'greeting'},
-    'Hello, world!'
-  );
+  useEffect(() => {
+    setInterval(() => {
+      interval(new Date().toLocaleTimeString())
+    }, 1000);
+  })
 
   return (
     <>
       <h1>Test env</h1>
-      {/* <section>{greet()}</section> */}
-
-      {/* {element1} */}
-      {/* ^ causes error */}
-
-      {element2}
+      <p>{time}</p>
     </>
   );
 }
